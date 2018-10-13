af9ea009a1971a81fa2de7b577e9e7fb86b0f51a
Josh Abraham <sinisterpatrician@gmail.com>
2018-10-13 14:31:38 -0400

    Add acct structure

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,28 @@ extern crate clap;
 
 use clap::{Arg, App};
 
+struct Acct_V3 {
+    ac_flag: u8,
+    ac_version: u8,
+    ac_tty: u16,
+    ac_exitcode: u32,
+    ac_uid: u32,
+    ac_gid: u32,
+    ac_pid: u32,
+    ac_ppid: u32,
+    ac_btime: u32,
+    ac_etime: f32,
+    ac_utime: u16,
+    ac_stime: u16,
+    ac_mem: u16,
+    ac_io: u16,
+    ac_rw: u16,
+    ac_minflt: u16,
+    ac_majflt: u16,
+    ac_swaps: u16,
+    ac_comm: [u8; 16],
+}
+
 // TODO: maybe use #[inline] here?
 fn expand_time(time: u16) -> u16 {
     let ret: u16 = (time & 0x1fff) << (((time >> 13) & 0x7) * 3);
