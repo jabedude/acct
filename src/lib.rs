6a340caa6d0d1c2499aedd588316a2ec0ceea851
Josh Abraham <sinisterpatrician@gmail.com>
2018-10-14 11:08:00 -0400

    Split main.rs into a lib and binary file

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,9 @@
 extern crate serde_derive;
 #[macro_use]
 extern crate bitflags;
-extern crate clap;
 extern crate bincode;
 
 
-use clap::{Arg, App};
 use bincode::deserialize;
 use std::fs::File;
 use std::io::Read;
@@ -122,7 +120,7 @@ fn is_file_valid_acct(file: &File) -> bool {
         .len() % mem::size_of::<AcctV3Inner>() as u64 == 0
 }
 
-fn load_from_file(file: &mut File) -> Option<Vec<AcctV3>> {
+pub fn load_from_file(file: &mut File) -> Option<Vec<AcctV3>> {
     if !is_file_valid_acct(&file) {
         return None;
     }
@@ -145,31 +143,4 @@ fn load_from_file(file: &mut File) -> Option<Vec<AcctV3>> {
     }
 
     Some(all)
-}
-
-fn main() {
-    let matches = App::new("acct-rs")
-                      .version("0.1")
-                      .author("Josh A. <sinisterpatrician@gmail.com>")
-                      .about("Parse acct(2) files")
-                      .arg(Arg::with_name("file")
-                           .short("f")
-                           .long("file")
-                           .value_name("FILE")
-                           .help("acct file to parse")
-                           .required(true)
-                           .takes_value(true))
-                      .get_matches();
-
-    let acct_file = matches.value_of("file").unwrap();
-    println!("file: {}", acct_file);
-
-
-    let mut file = File::open(acct_file).unwrap();
-    println!("file: {:?}", file);
-
-    let accts = load_from_file(&mut file).unwrap();
-    for acct in accts {
-        println!("{}", acct.command);
-    }
 }
